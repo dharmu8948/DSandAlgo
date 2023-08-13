94f84231cb7826b7451fef0eb7cd4c4d74d91d9a
Dharmendra Kumar <dksharma8115@gmail.com>
2023-08-13 10:59:58 +0530

    few Array Question added

--- Arrays/UniounOfTwoArray.cpp
+++ Arrays/UniounOfTwoArray.cpp
@@ -17,3 +17,43 @@ int main() {
 
     return 0;
 }
+/*
+
+// WAP to find union of two array
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int> arr{1,2,4,5,5,6,54};
+    vector<int> brr{1,3,5,4,6,6,7,54};
+    vector<int> ans;
+    for(int i=0;i<arr.size();i++) {
+        for(int j=0;j<brr.size();j++) {
+            if(arr[i] == brr[j]) {
+                brr[j] = -1;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<arr.size();i++) {
+        ans.push_back(arr[i]);
+    }
+    for(int i=0;i<brr.size();i++) {
+        if(brr[i] != -1)
+        ans.push_back(brr[i]);
+    }
+    cout<<"Printing elements"<<endl;
+    for(int i=0;i<ans.size();i++) {
+        cout<<ans[i]<<" ";
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+*/
