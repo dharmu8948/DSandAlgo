e44b3fd00e2d40b0b633a844882b85fb09277d07
Dharmendra Kumar <dksharma8115@gmail.com>
2023-08-16 20:16:21 +0530

    Modified version of Lucky Number from Leetcode added

--- Arrays/LuckyNumber.cpp
+++ Arrays/LuckyNumber.cpp
@@ -1,8 +1,18 @@
+/*
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+Return the largest lucky integer in the array. If there is no lucky integer return -1.
+
+	Input: arr = [2,2,3,4]
+	Output: 2
+	Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+*/
+
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
         // first approach using loop
-        int maxi = -1;
+     /*   int maxi = -1;
         for(int i=0;i<arr.size();i++) {
             int count = 0;
             for(int j=0;j<arr.size();j++) {
@@ -13,6 +23,18 @@ public:
             if(arr[i] == count)
             maxi = max(maxi, count);
         }
-        return maxi;
+        return maxi; */
+        // second approach with extra space
+        int maxi = -1;
+        unordered_map<int, int> hash;
+        for(int i=0;i<arr.size();i++) {
+            hash[arr[i]]++;
+        }
+       for(auto x : hash) {
+           if(x.first == x.second) {
+                maxi = max(maxi, x.second);
+           }
+       }
+       return maxi;
     }
 };
